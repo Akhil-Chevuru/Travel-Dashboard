dda57a68a256dd3572ccdd09dbf6004946c63a9f
Akhil-Chevuru <167983669+Akhil-Chevuru@users.noreply.github.com>
2025-09-19 00:33:27 +0530

    Fix React Fast Refresh production error properly

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    react({
+      fastRefresh: mode !== 'production',
+    }),
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+}));
